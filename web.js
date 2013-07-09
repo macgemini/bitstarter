20eeac29bac5c4de5d5766eb80ce653891e3e539
macgemini <gememacks@gmail.com>
2013-07-10 01:53:33 +0200

    change1

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var buffer = new Buffer(fs.readFileSync("index.html"), "utf-8")
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html").toString());
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
